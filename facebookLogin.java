4f1bae1bac70ee7ca4d077a7f065ecab852a127f
NagajothiS <32875392+NagajothiS@users.noreply.github.com>
2023-10-02 15:03:55 +0530

    Update facebookLogin.java

--- facebookLogin.java
+++ facebookLogin.java
@@ -1,29 +1,44 @@
-package week2.day1;
+package week1.Day5;
+
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class facebookLogin {
+public class FacebookRegistration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ChromeDriver driver1 = new ChromeDriver();
-		driver1.manage().window().maximize();
-		//EdgeDriver.driver2 = new EdgeDriver();
-		driver1.get("https://www.facebook.com/login");
-		//driver2.get("https://www.facebook.com/login");
-		WebElement email = driver1.findElement(By.id("email"));
-		email.sendKeys("testleaf.2023@gmail.com");
-		WebElement password = driver1.findElement(By.id("pass"));
-		password.sendKeys("Tuna@321"); 
-		WebElement login = driver1.findElement(By.id("loginbutton"));
-		login.click();
-		WebElement link = driver1.findElement(By.linkText("Find your account and log in."));
-		link.click();
-		String title = driver1.getTitle();
-		System.out.println("title: " + title);
+ ChromeDriver driver = new ChromeDriver();
+ driver.get("https://en-gb.facebook.com/");
+ driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+ driver.manage().window().maximize();
+ 
+ driver.findElement(By.linkText("Create new account")).click();
+ 
+ driver.findElement(By.name("firstname")).sendKeys("Jothi");
+ driver.findElement(By.name("lastname")).sendKeys("test");
+ driver.findElement(By.name("reg_email__")).sendKeys("9790667125");
+ driver.findElement(By.id("password_step_input")).sendKeys("Jothis1234");
+
+WebElement day = driver.findElement(By.id("day"));
+Select daydd = new Select(day);
+daydd.selectByIndex(29);
+
+WebElement month = driver.findElement(By.id("month"));
+Select monthdd = new Select(month);
+monthdd.selectByVisibleText("May");
+
+WebElement year = driver.findElement(By.id("year"));
+Select yeardd = new Select(year);
+yeardd.selectByVisibleText("1986");
+
+
+driver.findElement(By.linkText("female")).click();
+
+driver.findElement(By.name("websubmit")).click(); 
 	}
 
 }
